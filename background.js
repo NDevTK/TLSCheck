0d1327e37de0fca694029e2ed2e51271f8a6b15e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-12-05 16:00:58 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,23 @@
-browser.webRequest.onHeadersReceived.addListener(response => {
-  let info = browser.webRequest.getSecurityInfo(response.requestId, {certificateChain: true});
+async function sha256(data) {
+    const msgUint8 = new TextEncoder().encode(data + salt);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+browser.webRequest.onHeadersReceived.addListener(async response => {
+  let info = await browser.webRequest.getSecurityInfo(response.requestId, {certificateChain: true});
   if (info.state !== "secure") return
   let root = info.certificates[info.certificates.length - 1].fingerprint.sha256;
   let host = new URL(response.url).host;
-  let check = browser.storage.local.get(host)[host];
+  let hostHashed = await sha256(host);
+  let check = await browser.storage.local.get(hostHashed)[hostHashed];
   if (check === undefined) {
-    browser.storage.local.set({host, root});
+    await browser.storage.local.set({hostHashed, root});
   } else if (check !== root) {
     if (confirm('CA change for host: ' + host) === true) {
-      browser.storage.local.set({host, root});
+      await browser.storage.local.set({hostHashed, root});
     } else {
       return {cancel: true};
     }
